e7d5f4a2e55532167a586e340333bb2ced9647d8
Dan Dart <git@dandart.co.uk>
2020-11-12 00:12:17 +0000

    sls stuff

---
+++ src/Api.hs
@@ -0,0 +1,12 @@
+import AWSLambda.Events.APIGateway
+import Control.Lens
+import Data.Aeson.Embedded
+import Data.Time
+
+main :: IO ()
+main = apiGatewayMain handler
+
+handler :: APIGatewayProxyRequest (Embedded UTCTime) -> IO (APIGatewayProxyResponse String)
+handler request = do
+  print $ request ^. requestBody
+  pure $ responseOK & responseBody ?~ "OK"
