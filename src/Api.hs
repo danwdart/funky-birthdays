0fd71e04f8a6d0f33f31fa025f9459adabe45c50
Dan Dart <git@dandart.co.uk>
2020-11-29 11:31:39 +0000

    updates

--- src/Api.hs
+++ src/Api.hs
@@ -1,12 +1,13 @@
-import AWSLambda.Events.APIGateway
-import Control.Lens
-import Data.Aeson.Embedded
-import Data.Time
+{-# LANGUAGE UnicodeSyntax #-}
+import           AWSLambda.Events.APIGateway
+import           Control.Lens
+import           Data.Aeson.Embedded
+import           Data.Time
 
-main :: IO ()
+main ∷ IO ()
 main = apiGatewayMain handler
 
-handler :: APIGatewayProxyRequest (Embedded UTCTime) -> IO (APIGatewayProxyResponse String)
+handler ∷ APIGatewayProxyRequest (Embedded UTCTime) → IO (APIGatewayProxyResponse String)
 handler request = do
   print $ request ^. requestBody
   pure $ responseOK & responseBody ?~ "OK"
