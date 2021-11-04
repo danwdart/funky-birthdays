734b1fa5dc1c2e3313b7d0c092147f4cd1f88829
Dan Dart <git@dandart.co.uk>
2021-11-04 17:32:30 +0000

    Nixify

---
+++ default.nix
@@ -0,0 +1,56 @@
+{ 
+  nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
+  # https://github.com/chrra/iCalendar/issues/48
+  compiler ? "ghc901"
+} :
+let
+  gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  lib = nixpkgs.pkgs.haskell.lib;
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+      funky-birthdays = self.callCabal2nix "funky-birthdays" (gitignore ./.) {};
+      # Don't know why I need a doJailbreak in here, the version restriction of base
+      # looks up-to-date in the repository.
+      # https://github.com/stackbuilders/datetime/pull/13
+      datetime = lib.doJailbreak (self.callCabal2nix "datetime" (builtins.fetchGit {
+        url = "https://github.com/l29ah/datetime.git";
+        ref = "time-1.11";
+      }) {});
+      # Don't know why doJailbreak does nothing here?
+      # https://github.com/chrra/iCalendar/pull/46
+      iCalendar = lib.doJailbreak (self.callCabal2nix "iCalendar" (builtins.fetchGit {
+        url = "https://github.com/markus1189/iCalendar.git";
+        ref = "update-bounds";
+      }) {});
+    };
+  };
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.funky-birthdays
+    ];
+    shellHook = ''
+      gen-hie > hie.yaml
+      for i in $(find -type f); do krank $i; done
+    '';
+    buildInputs = with nixpkgs; with haskellPackages; [
+      apply-refact
+      cabal-install
+      ghcid
+      hlint
+      implicit-hie
+      krank
+      stan
+      stylish-haskell
+      weeder
+    ];
+    withHoogle = false;
+  };
+  exe = lib.justStaticExecutables (myHaskellPackages.funky-birthdays);
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit myHaskellPackages;
+  funky-birthdays = myHaskellPackages.funky-birthdays;
+}
+
