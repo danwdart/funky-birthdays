4e5cadc19e17a3b28eed35c0d32f47bdf5d70019
Dan Dart <git@dandart.co.uk>
2025-05-31 19:05:33 +0100

    split into echo and write

---
+++ src/echoCal.hs
@@ -0,0 +1,13 @@
+module Main (main) where
+
+import Data.ByteString.Lazy.Char8 qualified as BSL
+import Data.Map
+import Data.Time.Clock
+import ICalendar
+import Utils                      (untilEnd)
+
+main ∷ IO ()
+main = do
+    now <- getCurrentTime
+    let ical = generateICal . fromList . untilEnd $ now
+    BSL.putStr ical
