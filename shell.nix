fc8466eb0a315606c76c281466eefbdf97c6efb1
Dan Dart <git@dandart.co.uk>
2025-06-08 22:39:07 +0100

    bulidables

--- shell.nix
+++ shell.nix
@@ -5,14 +5,31 @@ mkShell rec {
         haskell.compiler.ghc912
         cabal-install
         krank
-        pkg-config
-        zlib.dev
-        pcre.dev
+        # pkg-config
+        # zlib.dev
+        # pcre.dev
+        # for gtk version
+        # webkitgtk_4_0.dev # only this old version?
+        # gobject-introspection.dev
+        # libsysprof-capture
+        # pcre2.dev
+        # util-linux.dev
+        # libselinux.dev
+        # libsepol.dev
+        # libthai.dev
+        # libdatrie.dev
+        # xorg.libXdmcp.dev
+        # lerc.dev
+        # libxkbcommon.dev
+        # libepoxy.dev
+        # xorg.libXtst
+        # sqlite.dev
+        # libpsl.dev
     ];
     shellHook = ''
         [[ -f ~/.local/bin/refactor ]] || cabal install apply-refact cabal-fmt doctest ghci-dap ghcid ghcide haskell-debug-adapter haskell-language-server hasktags hlint hoogle hpack implicit-hie stan stylish-haskell weeder --overwrite-policy=always --allow-newer
         export PATH=~/.local/bin:$PATH
         gen-hie > hie.yaml
-        for i in $(find -type f | grep -v dist-newstyle); do krank $i; done
+        for i in $(find -type f | grep -v "dist-*"); do krank $i; done
     '';
 }
